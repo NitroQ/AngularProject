e2aa069e8ffb02ef94a921797459f8c6f2816830
dumpit-ca <dumpitca@outlook.com>
2022-10-23 13:52:22 +0800

    made navbar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  list = [
+    {
+      number: '1',
+      name: 'Home',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      number: '2',
+      name: 'Explore',
+      icon: 'fa-solid fa-compass',
+    },
+    {
+      number: '3',
+      name: 'Consult',
+      icon: 'fa-solid fa-calendar',
+    },
+    {
+      number: '4',
+      name: 'Contact',
+      icon: 'fa-solid fa-circle-question',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
