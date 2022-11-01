17c13fcb8cd9dac834a361c0fec6485c478bd659
dumpit-ca <dumpitca@outlook.com>
2022-11-01 13:33:48 +0800

    created separated consult forms

---
+++ src/app/components/consult-forms/consult-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-forms',
+  templateUrl: './consult-forms.component.html',
+  styleUrls: ['./consult-forms.component.scss']
+})
+export class ConsultFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
