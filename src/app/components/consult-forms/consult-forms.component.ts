e44119769822c358a02c62152ab39f2866c02db2
dumpit-ca <dumpitca@outlook.com>
2022-11-06 02:24:05 +0800

    finished popup modal onclick submit btn

--- src/app/components/consult-forms/consult-forms.component.ts
+++ src/app/components/consult-forms/consult-forms.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/bs-datepicker.config';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-consult-forms',
@@ -60,5 +61,27 @@ export class ConsultFormsComponent implements OnInit {
   }
   previous() {
     this.step = this.step - 1;
+  }
+  btnSuccess() {
+    Swal.fire({
+      title: 'BOOKING CONFIRMATION',
+      html: '<p>You have successfully booked a consultation with Nibs and Knobs! A meeting invitation will be sent in your email.</p> <img class="img-fluid my-2" src="../../../assets/images/consult-images/check-icon.png">',
+      // imageUrl: '../../../assets/images/consult-images/check-icon.png',
+      background: `#1a1a1a`,
+      width: '40%',
+      buttonsStyling: false,
+      confirmButtonText: 'Okay',
+      reverseButtons: true,
+      customClass: {
+        title: 'SoraBold orange',
+        // image: 'img-fluid my-5',
+        popup: 'popup-border py-4 px-3',
+        htmlContainer: 'white',
+        actions: 'd-flex w-100',
+        confirmButton: 'ms-auto me-4 btn mat-stroked-button rounded-pill',
+
+        // popup: 'px-5'
+      },
+    });
   }
 }
