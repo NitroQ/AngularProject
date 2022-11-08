0ae3bed9316b6e1de712be47dd012a24e45de567
dumpit-ca <dumpitca@outlook.com>
2022-11-08 19:00:07 +0800

    created success message modal

--- src/app/components/contact-forms/contact-forms.component.ts
+++ src/app/components/contact-forms/contact-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-forms',
@@ -9,7 +10,25 @@ export class ContactFormsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  btnSubmit() {
+    Swal.fire({
+      title: 'THANK YOU FOR MESSAGING US',
+      html: '<p>Your message has been successfully sent to Nibs and Knobs! Our team will get back to you soon!</p> <img class=" w-50 h-75 my-2" src="../../../assets/images/contact-images/email-icon.png">',
+      background: `#1a1a1a`,
+      width: '40%',
+      buttonsStyling: false,
+      confirmButtonText: 'Okay',
+      reverseButtons: true,
+      customClass: {
+        title: 'SoraBold orange',
+        popup: 'popup-border py-4 px-3',
+        htmlContainer: 'white',
+        actions: 'd-flex w-100',
+        confirmButton: 'ms-auto me-4 btn mat-stroked-button rounded-pill',
+      },
+    });
+  }
 }
+
