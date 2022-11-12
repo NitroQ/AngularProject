3b98f2d33b97f5a914f1d444a0b30f15c210ae65
dumpit-ca <dumpitca@outlook.com>
2022-11-12 22:26:29 +0800

    changed styling for confirmation message

--- src/app/public/components/consult/consult.component.ts
+++ src/app/public/components/consult/consult.component.ts
@@ -9,22 +9,6 @@ import Swal from 'sweetalert2';
 export class ConsultComponent implements OnInit {
   // * * when book button is click, confirmation message will appear
   btnBook() {
-    // Swal.fire({
-    //   title: 'BOOKING CONFIRMATION',
-    //   html: '<p>You have successfully booked a consultation with Nibs and Knobs! A meeting invitation will be sent in your email.</p> <img class="img-fluid my-2" src="../../../assets/images/consult-images/check-icon.png">',
-    //   background: `#FFFFFF`,
-    //   width: '40%',
-    //   buttonsStyling: false,
-    //   confirmButtonText: 'OKAY!',
-    //   reverseButtons: true,
-    //   customClass: {
-    //     title: 'SoraBold orange',
-    //     popup: 'popup-border py-4 px-3',
-    //     htmlContainer: '#343434',
-    //     actions: 'd-flex w-100',
-    //     confirmButton: 'btn-modal px-4 py-2 rounded-pill mx-auto d-block text-uppercase',
-    //   },
-    // });
     Swal.fire({
       title: 'Your Booking is CONFIRMED',
       imageUrl: '../../../assets/images/consult-images/check-icon.png',
