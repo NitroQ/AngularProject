ba1bef0e44f468d2449feaf3518002baaea1dee7
dumpit-ca <dumpitca@outlook.com>
2022-11-12 22:29:05 +0800

    fixed screen height for window.scroll

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class NavbarComponent implements OnInit {
   navbarfixed: boolean = false;
   @HostListener('window:scroll', ['$event']) onScroll() {
-    if (window.scrollY > 190) {
+    if (window.scrollY > 100) {
       this.navbarfixed = true;
     } else {
       this.navbarfixed = false;
