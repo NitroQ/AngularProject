c57fb70e4455c609f59413818db048ba61ccea71
NitroQ <frankgesmundo10@gmail.com>
2022-11-13 00:16:38 +0800

    consult working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PublicModule } from './public/public.module';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule, PublicModule, RouterModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule, FormsModule, HttpClientModule,  PublicModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -8,8 +8,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { ExploreGalleryComponent } from './components/explore-gallery/explore-gallery.component';
 import { ExploreComponent } from './components/explore/explore.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [PublicComponent, HomeComponent, ConsultComponent, ContactComponent, ExploreGalleryComponent, ExploreComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule, FormsModule, ReactiveFormsModule, HttpClientModule]
 })
 export class PublicModule {}
