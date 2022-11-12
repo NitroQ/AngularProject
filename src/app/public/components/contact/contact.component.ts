e1a336ad097082a0043791e07c14941d660c05ec
dumpit-ca <dumpitca@outlook.com>
2022-11-12 22:27:09 +0800

    added confirmtation message for submit message

--- src/app/public/components/contact/contact.component.ts
+++ src/app/public/components/contact/contact.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  btnSubmit() {
+    Swal.fire({
+      title: 'Your Booking is CONFIRMED',
+      imageUrl: '../../../assets/images/contact-images/email-icon.png',
+      html: 'Your message has been successfully sent to Nibs and Knobs! Our team will get back to you soon!',
+      showCancelButton: false,
+      focusConfirm: false,
+      confirmButtonText: 'OKAY!',
+      buttonsStyling: false,
+      customClass: {
+        title: 'SoraBold orange',
+        popup: 'popup-border py-4 px-3',
+        htmlContainer: '#343434',
+        actions: 'd-flex w-100',
+        confirmButton: 'btn-modal px-4 py-2 rounded-pill mx-auto d-block text-uppercase',
+      },
+    });
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
