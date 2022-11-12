044f3f94645747149b7cd2db6bfe0f119766acfb
dumpit-ca <dumpitca@outlook.com>
2022-11-12 22:41:34 +0800

    created explore-gallery page

---
+++ src/app/public/components/explore-gallery/explore-gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-gallery',
+  templateUrl: './explore-gallery.component.html',
+  styleUrls: ['./explore-gallery.component.scss']
+})
+export class ExploreGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
