1c51b036df37c9a9ca774656d22926b650b475dd
dumpit-ca <dumpitca@outlook.com>
2022-11-12 18:04:01 +0800

    removed uncessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// angular material UI
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatStepperModule } from '@angular/material/stepper';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CarouselModule } from 'ngx-owl-carousel-o';
 import { PublicModule } from './public/public.module';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatToolbarModule,
-    MatDividerModule,
-    MatIconModule,
-    BsDatepickerModule.forRoot(),
-    TimepickerModule.forRoot(),
-    ReactiveFormsModule,
-    CarouselModule,
-    PublicModule,
-    RouterModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule, PublicModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
