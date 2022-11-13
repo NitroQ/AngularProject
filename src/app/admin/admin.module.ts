20ace7f415ca2dca8272635e6c216c42c6735bd4
dumpit-ca <dumpitca@outlook.com>
2022-11-13 16:12:15 +0800

    added sidenav to admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,9 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [DashboardComponent, AdminComponent],
-  imports: [CommonModule, RouterModule, AdminRoutingModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    AdminRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
 export class AdminModule {}
