b894878dd2b0700f25b3c8671bc3bd73c94a331a
dumpit-ca <dumpitca@outlook.com>
2022-11-15 00:43:40 +0800

    added confirmatiom button

--- src/app/admin/components/add/add-design/add-design.component.ts
+++ src/app/admin/components/add/add-design/add-design.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-design',
   templateUrl: './add-design.component.html',
-  styleUrls: ['./add-design.component.scss']
+  styleUrls: ['./add-design.component.scss'],
 })
 export class AddDesignComponent implements OnInit {
+  files: File[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(event: { addedFiles: any }) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
   }
 
+  onRemove(event: File) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+  addDesignDetails() {
+    Swal.fire({
+      title: 'Are you sure?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Add',
+      denyButtonText: `Don't add`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Successfully added!', '', 'success');
+      } else if (result.isDenied) {
+        Swal.fire('No design added', '', 'info');
+      }
+    });
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
